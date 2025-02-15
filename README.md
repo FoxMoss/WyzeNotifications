97594e660f8b9187f4f920aeb87662201e6bf980
FoxMoss <foxmoss@mediaology.com>
2025-02-15 16:53:15 -0600

    feat: notifications

---
+++ README.md
@@ -0,0 +1,14 @@
+# Wyze Notification
+Why? Because they dont work in the app sometimes.
+
+Create an API key [here](https://developer-api-console.wyze.com/#/apikey/view).
+
+## Example .env
+
+```
+WYZE_EMAIL=myemail@example.com
+WYZE_PASSWORD=Hi.Hackers.5
+KEY_ID=6faa5dca-ebef-11ef-b499-ef5e917aa7ab
+API_KEY=...
+NTFY_CODE=my-unique-doorbell-name
+```
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import time
+import requests
+from wyze_sdk import Client
+from wyze_sdk.models.events import EventAlarmType
+from dotenv import load_dotenv
+from datetime import timedelta, datetime
+load_dotenv()
+
+client = Client()
+response = client.login(
+    email=os.getenv('WYZE_EMAIL'),
+    password=os.getenv('WYZE_PASSWORD'),
+    key_id=os.getenv('KEY_ID'),
+    api_key=os.getenv('API_KEY'))
+
+access_token = response['access_token']
+refresh_token = response['refresh_token']
+
+
+read_messages = []
+reset_counter = 0
+while True:
+    for event in client.events.list(begin=datetime.now() - timedelta(minutes=10)):
+        if (event.alarm_type == EventAlarmType.MOTION or event.alarm_type == EventAlarmType.DOORBELL_RANG) and not (event.id in read_messages):
+            print(event.id)
+            attach = ""
+            if (len(event.files) > 0):
+                attach = event.files[0].url
+            requests.post("https://ntfy.sh/" + os.getenv('NTFY_CODE'),
+                          data=event.alarm_type.describe() + " event triggered.",
+                          headers={
+                              "Title": "Doorbell rang!",
+                              "Priority": "urgent",
+                              "Tags": "warning",
+                              "Attach": attach
+            })
+            read_messages.append(event.id)
+    reset_counter += 1
+    if reset_counter > 10*60:
+        read_messages = []
+        reset_counter = 0
+    time.sleep(1)
